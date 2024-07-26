a910878bc9f7f38fc3ff8ff866e34b9f861a5e17
a39849686 <a0967067696@gmail.com>
2024-07-26 21:03:41 +0800

    new practice on jaxws

--- 01_jaxws_server/src/test/java/com/roger/Server.java
+++ 01_jaxws_server/src/test/java/com/roger/Server.java
@@ -1,7 +1,10 @@
 package com.roger;
 
 import com.roger.service.impl.HelloServiceImpl;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Roger
@@ -9,6 +12,9 @@ import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
  * @create 2024-07-26
  */
 public class Server {
+
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) {
         // 發布服務的工廠
         JaxWsServerFactoryBean factoryBean = new JaxWsServerFactoryBean();
@@ -19,9 +25,14 @@ public class Server {
         // 設置服務類
         factoryBean.setServiceBean(new HelloServiceImpl());
 
+        // 添加日誌輸入，輸出攔截器，觀察 soap 請求以及 soap 回應的內容
+        factoryBean.getInInterceptors().add(new LoggingInInterceptor());
+        factoryBean.getOutInterceptors().add(new LoggingInInterceptor());
+
         // 發布服務
         factoryBean.create();
 
+        logger.info("發布服務成功，端口 8000");
         System.out.println("發布服務成功，端口 8000");
     }
 }
---
+++ 03_jaxws_spring_server/src/main/java/com/roger/service/impl/HelloServiceImpl.java
@@ -0,0 +1,15 @@
+package com.roger.service.impl;
+
+import com.roger.service.HelloService;
+
+/**
+ * @author Roger
+ * @Description
+ * @create 2024-07-26
+ */
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String sayHello(String name) {
+        return name + "，Welcome to Roger Home.";
+    }
+}
